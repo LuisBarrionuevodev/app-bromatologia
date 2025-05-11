bf73b6f56840116f821e0294ff714c823f6dcafb
LuisBarrionuevodev <luisbarrionuevo.dev@gmail.com>
2025-05-11 18:55:36 -0300

    Primer commit del proyecto

---
+++ app/models/contribuyente.py
@@ -0,0 +1,13 @@
+import mysql.connector
+from app.database.config import obtener_conexion
+
+
+class Contribuyente:
+    def _init_(self,nombre,apellido,cuit,sexo):
+        self.nombre= nombre
+        self.apellido= apellido
+        self.cuit=cuit
+        self.sexo=sexo
+
+        def create(nombre,apellido,cuit, sexo):
+            
